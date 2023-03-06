bb9248f85d8ef9326bdbb790d12db3b1a3280ec7
bibhashhalder <halderbibhash@gmail.com>
2023-03-06 12:16:21 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+import './App.css';
+import TopButton from './component/TopButton';
+
+function App() {
+  return (
+    <div className='max-w-screen-md px-32 py-5 mx-auto mt-4 shadow-xl bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-gray-700'>
+    <TopButton></TopButton>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/TopButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopButton = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TopButton;
